f7fd3ea3da17bab885e577cc200a0b3a869144d8
Brianceasar <95188858+Brianceasar@users.noreply.github.com>
2023-12-14 12:29:52 +0300

    close #3

--- php/submit_invoice.php
+++ php/submit_invoice.php
@@ -1,27 +1,31 @@
 <?php
-// Assuming you have a MySQL database set up
+
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "byte_customers";
+$dbname = "byte_customers"; 
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Process form submission
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Validate and sanitize the data (you may add more validation as needed)
+    // Process form data
     $clientName = mysqli_real_escape_string($conn, $_POST['clientName']);
-    $itemDescription = mysqli_real_escape_string($conn, $_POST['itemDescription']);
-    $itemCost = floatval($_POST['itemCost']);
+    
+    // Process itemized list
+    $itemDescriptions = $_POST['itemDescription'];
+    $itemCosts = $_POST['itemCost'];
+
+    // Calculate totals
+    $subtotal = array_sum($itemCosts);
+    $tax = $subtotal * 0.1; // Assuming 10% tax, adjust as needed
+    $grandTotal = $subtotal + $tax;
 
     // Insert data into the database
-    $sql = "INSERT INTO invoices (client_name, item_description, item_cost) VALUES ('$clientName', '$itemDescription', $itemCost)";
+    $sql = "INSERT INTO invoices (client_name, subtotal, tax, grand_total) VALUES ('$clientName', $subtotal, $tax, $grandTotal)";
 
     if ($conn->query($sql) === TRUE) {
         echo "Invoice submitted successfully!";
@@ -30,6 +34,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 
-// Close the connection
 $conn->close();
+
 ?>
